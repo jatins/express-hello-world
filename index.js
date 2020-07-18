3d1d3568f1482bb2743eea097ea789dffe5a4203
drtobbyas <dr.tobbyas@gmail.com>
2020-07-18 11:29:14 +0100

    add shipit

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.get('/', function (req, res) {
 })
 
 app.listen(3001, function () {
-  console.log('Listening on port 3000...')
+  console.log('Listening on port 3001...')
 })
---
+++ shipitfile.js
@@ -0,0 +1,81 @@
+module.exports = (shipit) => {
+  require("shipit-deploy")(shipit);
+  require("shipit-shared")(shipit);
+
+  const appName = "hello";
+
+  shipit.initConfig({
+    default: {
+      deployTo: "/home/drtob/hello",
+      repositoryUrl: "https://github.com/drtobbyas/express-hello-world.git",
+      keepReleases: 3,
+      shared: {
+        overwrite: true,
+        dirs: ["node_modules"],
+      },
+    },
+    production: {
+      servers: "drtob@206.81.15.85",
+    },
+  });
+
+  const path = require("path");
+  const ecosystemFilePath = path.join(
+    shipit.config.deployTo,
+    "shared",
+    "ecosystem.config.js"
+  );
+
+  // Our listeners and tasks will go here
+
+  shipit.on("updated", () => {
+    shipit.start("npm-install", "copy-config");
+  });
+
+  shipit.on("published", () => {
+    shipit.start("pm2-server");
+  });
+
+  shipit.blTask("copy-config", async () => {
+    const fs = require("fs");
+
+    const ecosystem = `
+module.exports = {
+apps: [
+  {
+    name: '${appName}',
+    script: '${shipit.releasePath}/index.js',
+    watch: true,
+    autorestart: true,
+    restart_delay: 1000,
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }
+]
+};`;
+
+    fs.writeFileSync("ecosystem.config.js", ecosystem, function (err) {
+      if (err) throw err;
+      console.log("File created successfully.");
+    });
+
+    await shipit.copyToRemote("ecosystem.config.js", ecosystemFilePath);
+  });
+
+  
+shipit.blTask('npm-install', async () => {
+  shipit.remote(`cd ${shipit.releasePath} && yarn install --production`);
+});
+
+shipit.blTask('pm2-server', async () => {
+  await shipit.remote(`pm2 delete -s ${appName} || :`);
+  await shipit.remote(
+    `pm2 start ${ecosystemFilePath} --env production --watch true`
+  );
+});
+
+};
