9c6695739676d5f0f1bec5466ff8048ec48e29a2
drtobbyas <dr.tobbyas@gmail.com>
2020-08-10 20:09:32 +0100

    test(): adds new test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World! from app2 and ...and ---')
+  res.json({ message: 'Hello World!' })
 })
 
 app.listen(3002, function () {
